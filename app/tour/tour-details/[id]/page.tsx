8f75c06492f922e0ec4aecae56f2942cb03cda46
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-07-17 10:34:00 +0600

    working on routing test

---
+++ app/tour/tour-details/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TourDetailsPage = () => {
+    return (
+        <div>
+            <h4 className='text-4xl'>Tour Details Page Dynamic</h4>
+        </div>
+    );
+};
+
+export default TourDetailsPage;
